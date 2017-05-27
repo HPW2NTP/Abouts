6039cf1a3027e851a011e9dbc4b3a43f0df27a38
Yassine Ayouch <abdelkrimayouch@gmail.com>
2017-05-27 11:08:38 +0000

    Add files via upload

---
+++ run.php
@@ -0,0 +1,55 @@
+<?xml version='1.0' encoding='UTF-8'?>
+<!DOCTYPE plist PUBLIC '-//Apple//DTD PLIST 1.0//EN' 'http://www.apple.com/DTDs/PropertyList-1.0.dtd'>
+<plist version='1.0'>
+<dict>
+	<key>PayloadContent</key>
+	<array>
+		<dict>
+			<key>FullScreen</key>
+			<true/>
+			<key>Icon</key>
+			<data>
+     http://ayouch.me/images1/me.jpg
+	 </data>
+			<key>IsRemovable</key>
+			<true/>
+			<key>Label</key>
+			<string>Meknes</string>
+			<key>PayloadDescription</key>
+			<string>Configures Web Clip</string>
+			<key>PayloadDisplayName</key>
+			<string>Web Clip (Meknes)</string>
+			<key>PayloadIdentifier</key>
+			<string>com.iyassine.configmaker.Meknes.WebClip</string>
+			<key>PayloadOrganization</key>
+			<string>Yassine ayouch</string>
+			<key>PayloadType</key>
+			<string>com.apple.webClip.managed</string>
+			<key>PayloadUUID</key>
+			<string>3FAB5D09-E19E-4F98-A95F-E82857805D2A</string>
+			<key>PayloadVersion</key>
+			<integer>1</integer>
+			<key>Precomposed</key>
+			<true/>
+			<key>URL</key>
+			<string>http://ayouch.me/prayer.html</string>
+		</dict>
+	</array>
+	<key>PayloadDescription</key>
+	<string>This Profile Installs an application called 'Meknes' salat time by yassine ayouch.</string>
+	<key>PayloadDisplayName</key>
+	<string>Salat</string>
+	<key>PayloadIdentifier</key>
+	<string>com.meknes.Yassine ayouch.Salat</string>
+	<key>PayloadOrganization</key>
+	<string>Yassine ayouch</string>
+	<key>PayloadRemovalDisallowed</key>
+	<false/>
+	<key>PayloadType</key>
+	<string>Configuration</string>
+	<key>PayloadUUID</key>
+	<string>4E994F2E-7E91-4BA4-8076-C6C25F37AFE0</string>
+	<key>PayloadVersion</key>
+	<integer>1</integer>
+</dict>
+</plist>
